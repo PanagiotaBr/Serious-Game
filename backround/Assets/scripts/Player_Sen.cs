effd0b7f7de2c2e80eba8fabcd8c1d655acd4c23
Danai Konstantakou <danaikon9@gmail.com>
2025-05-14 12:28:13 +0300

    Serentiy background + infity level

--- backround/Assets/scripts/Player_Sen.cs
+++ backround/Assets/scripts/Player_Sen.cs
@@ -5,17 +5,27 @@ public class Player_Sen : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D body;
     private Animator anim;
+    private Vector3 respawnPoint;
 
     private void Awake() 
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        respawnPoint = transform.position;
     }
 
     private void Update()
     {
         body.linearVelocity = new Vector2(speed, body.linearVelocity.y);
         transform.localScale = Vector3.one;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+    	if (collision.gameObject.CompareTag("Respawn"))
+        {
+            transform.position = respawnPoint;
+        }
     }
 
 }
