49b6f56d4355e5d0057ae670cd8862ba890debeb
Danai Konstantakou <danaikon9@gmail.com>
2025-05-17 11:31:58 +0300

    audio loudness

--- backround/Assets/scripts/Scale_Loudness.cs
+++ backround/Assets/scripts/Scale_Loudness.cs
@@ -3,26 +3,38 @@ using UnityEngine;
 public class Scale_Loudness : MonoBehaviour
 {
     // public AudioSource source;
-    public Vector2 minScale;
-    public Vector2 maxScale;
+    public Vector3 minScale;
+    public Vector3 maxScale;
     public AudioLoudnessDetection detector;
     public float loudnessSensibility = 100;
     public float threshold = 0.1f;
+    public float ypos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ypos = transform.position.y;
     }
 
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         float loudness = detector.GetLoudnessFromMicrophone() * loudnessSensibility;
-        if (loudness < threshold)
-            loudness = 0;
-        transform.localScale = Vector2.Lerp(minScale, maxScale, loudness);
-        // Debug.Log("Loudness: " + loudness);
+        if (loudness > threshold)
+        {
+            transform.localScale = maxScale;
+            transform.position = new Vector3(transform.position.x,-2, transform.position.z);
+            // Debug.Log("Scaling UP");
+        }
+        else
+        {
+            transform.localScale = minScale;
+            transform.position = new Vector3(transform.position.x, ypos, transform.position.z);
+            // Debug.Log("Scalin/g DOWN");
+        }
+        // transform.localScale = Vector2.Lerp(minScale, maxScale, loudness);
+        Debug.Log("Loudness: " + loudness);
+        // Debug.Log("Scale: " + transform.localScale);
     }
 }
